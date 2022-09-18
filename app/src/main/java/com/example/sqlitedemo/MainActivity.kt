958df9664f3d1d5989544ced625098c80bc8a937
devprincefahad <1pfchouhan@gmail.com>
2022-09-19 02:21:56 +0530

    SQLiteDemo

---
+++ app/src/main/java/com/example/sqlitedemo/MainActivity.kt
@@ -0,0 +1,197 @@
+package com.example.sqlitedemo
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAdd: Button
+    lateinit var rvItemsList: RecyclerView
+    lateinit var tvNoRecordsAvailable: TextView
+    lateinit var etName: EditText
+    lateinit var etEmailId: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        findIDs()
+
+        btnAdd.setOnClickListener { view ->
+            addRecord()
+        }
+
+        setupListofDataIntoRecyclerView()
+    }
+
+    private fun findIDs() {
+        btnAdd = findViewById(R.id.btnAdd)
+        rvItemsList = findViewById(R.id.rvItemsList)
+        tvNoRecordsAvailable = findViewById(R.id.tvNoRecordsAvailable)
+        etName = findViewById(R.id.etName)
+        etEmailId = findViewById(R.id.etEmailId)
+    }
+
+    /**
+     * Method is used to show the list of inserted data.
+     */
+    private fun setupListofDataIntoRecyclerView() {
+
+        if (getItemsList().size > 0) {
+
+            rvItemsList.visibility = View.VISIBLE
+            tvNoRecordsAvailable.visibility = View.GONE
+
+            // Set the LayoutManager that this RecyclerView will use.
+            rvItemsList.layoutManager = LinearLayoutManager(this)
+            // Adapter class is initialized and list is passed in the param.
+            val itemAdapter = ItemAdapter(this, getItemsList())
+            // adapter instance is set to the recyclerview to inflate the items.
+            rvItemsList.adapter = itemAdapter
+        } else {
+
+            rvItemsList.visibility = View.GONE
+            tvNoRecordsAvailable.visibility = View.VISIBLE
+        }
+    }
+
+    /**
+     * Method is used to get the Items List from the database table.
+     */
+    private fun getItemsList(): ArrayList<EmpModelClass> {
+        //creating the instance of DatabaseHandler class
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        //calling the viewEmployee method of DatabaseHandler class to read the records
+        val empList: ArrayList<EmpModelClass> = databaseHandler.viewEmployee()
+
+        return empList
+    }
+
+    //Method for saving the employee records in database
+    private fun addRecord() {
+        val name = etName.text.toString()
+        val email = etEmailId.text.toString()
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        if (!name.isEmpty() && !email.isEmpty()) {
+            val status =
+                databaseHandler.addEmployee(EmpModelClass(0, name, email))
+            if (status > -1) {
+                Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
+                etName.text.clear()
+                etEmailId.text.clear()
+
+                setupListofDataIntoRecyclerView()
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    /**
+     * Method is used to show the Custom Dialog.
+     */
+    fun updateRecordDialog(empModelClass: EmpModelClass) {
+
+        val updateDialog = Dialog(this, R.style.Theme_Dialog)
+
+        updateDialog.setCancelable(false)
+        /*Set the screen content from a layout resource.
+         The resource will be inflated, adding all top-level views to the screen.*/
+        updateDialog.setContentView(R.layout.dialog_update)
+        val etUpdateName = updateDialog.findViewById<EditText>(R.id.etUpdateName)
+        val etUpdateEmailId = updateDialog.findViewById<EditText>(R.id.etUpdateEmailId)
+        val tvUpdate = updateDialog.findViewById<TextView>(R.id.tvUpdate)
+        val tvCancel = updateDialog.findViewById<TextView>(R.id.tvCancel)
+        //updateDialog.etUpdateName.setText(empModelClass.name)
+        etUpdateName.setText(empModelClass.name)
+        etUpdateEmailId.setText(empModelClass.email)
+
+        tvUpdate.setOnClickListener(View.OnClickListener {
+
+            val name = etUpdateName.text.toString()
+            val email = etUpdateEmailId.text.toString()
+
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+
+            if (!name.isEmpty() && !email.isEmpty()) {
+                val status =
+                    databaseHandler.updateEmployee(EmpModelClass(empModelClass.id, name, email))
+                if (status > -1) {
+                    Toast.makeText(applicationContext, "Record Updated.", Toast.LENGTH_LONG).show()
+
+                    setupListofDataIntoRecyclerView()
+
+                    updateDialog.dismiss() // Dialog will be dismissed
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Name or Email cannot be blank",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+        tvCancel.setOnClickListener(View.OnClickListener {
+            updateDialog.dismiss()
+        })
+        //Start the dialog and display it on screen.
+        updateDialog.show()
+    }
+
+    /**
+     * Method is used to show the Alert Dialog.
+     */
+    fun deleteRecordAlertDialog(empModelClass: EmpModelClass) {
+        val builder = AlertDialog.Builder(this)
+        //set title for alert dialog
+        builder.setTitle("Delete Record")
+        //set message for alert dialog
+        builder.setMessage("Are you sure you wants to delete ${empModelClass.name}.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        //performing positive action
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+
+            //creating the instance of DatabaseHandler class
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+            //calling the deleteEmployee method of DatabaseHandler class to delete record
+            val status = databaseHandler.deleteEmployee(EmpModelClass(empModelClass.id, "", ""))
+            if (status > -1) {
+                Toast.makeText(
+                    applicationContext,
+                    "Record deleted successfully.",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                setupListofDataIntoRecyclerView()
+            }
+
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        //performing negative action
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
+    }
+}
